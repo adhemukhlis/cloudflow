66eee510d29b625eb5000caf2d575f70f3644167
adhemukhlis <adhemukhlis@gmail.com>
2023-07-30 15:06:36 +0700

    add antd

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from '@/src/App'
 import reportWebVitals from './reportWebVitals'
+import { ReactFlowProvider } from 'reactflow'
+import { StyleProvider } from '@ant-design/cssinjs'
+import { ConfigProvider, theme } from 'antd'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+	<StyleProvider hashPriority="high">
+		<ConfigProvider
+			theme={{
+				algorithm: theme.darkAlgorithm
+			}}>
+			<ReactFlowProvider>
+				<App />
+			</ReactFlowProvider>
+		</ConfigProvider>
+	</StyleProvider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
