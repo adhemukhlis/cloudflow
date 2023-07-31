d4956c440680913337c79389d4496e01cd3af196
Martarion-Code <mharionmarterl@gmail.com>
2023-07-31 20:33:59 +0700

    Modify and add feature in  CsvUploadNode
    modify dragging file so that the node  not flickering even though the user dragged the file  inside node, also I added name of the file when user dropped the file

--- src/Custom Nodes/Input/CsvUploadNode.js
+++ src/Custom Nodes/Input/CsvUploadNode.js
@@ -1,27 +1,24 @@
 import React, { useRef, useState } from 'react'
 import { Handle, Position } from 'reactflow'
-// import { parse } from 'csv-parse'
 
-// import csvParse from 'csv-parse';
-// import { parse } from 'csv-parse';
 
 function CsvUploadNode({ id, data }) {
 	const inputEl = useRef(null)
 
-	useState
+	const [file, setFile] = useState(null)
 	const [isDragging, setIsDragging] = useState(false)
 
 	const handleDragEnter = (e) => {
 		e.preventDefault()
 		if (!e.relatedTarget || !e.currentTarget.contains(e.relatedTarget)) {
-			setIsDragging(true);
-		  }
+			setIsDragging(true)
+		}
 	}
 	const handleDragLeave = (e) => {
 		e.preventDefault()
 		if (!e.relatedTarget || !e.currentTarget.contains(e.relatedTarget)) {
-			setIsDragging(false);
-		  }
+			setIsDragging(false)
+		}
 	}
 	const handleDragOver = (e) => {
 		e.preventDefault()
@@ -32,13 +29,12 @@ function CsvUploadNode({ id, data }) {
 		e.preventDefault()
 		setIsDragging(false)
 		const file = e.dataTransfer.files[0]
-		const csvData = await fetch(file).then((response) => response.text())
-
+		setFile(file)
 	}
-	// Use the readable stream api to consume records
+	
 
 	const handleFileInputChange = (e) => {
-		console.log(e.target.files)
+		setFile(file)
 	}
 	return (
 		<div
@@ -46,34 +42,39 @@ function CsvUploadNode({ id, data }) {
 			onDragEnter={handleDragEnter}
 			onDragLeave={handleDragLeave}
 			onDragOver={handleDragOver}
-			onDrop={handleDragDrop}
-			>
+			onDrop={handleDragDrop}>
 			<div className={`detail-node ${isDragging ? 'active' : ''}`}>
 				<div className="heading-node input-file-node__heading">
 					<h5 className="heading-node__title">File</h5>
 					<div className="close">X</div>
 				</div>
 				<div className="input-file-node__input-file-cont">
+				{!file ? (
+					<>
 					<div className="input-file-cont__drop-and-file-dialog-cont">
-						<p className="drop-and-file-dialog-cont__drop-file-text">
-							{/* {isDragging ? 'Drop the file here or ' : 'Drag file here or'}{' '} */}
-							Drop the file here or
-						</p>
-						<button className="drop-and-file-dialog-cont__open-file-dialog-btn" onClick={() => inputEl.current.click()}>
-							Open File Dialog
-						</button>
+							<p className="drop-and-file-dialog-cont__drop-file-text">
+								Drop the file here or
+							</p>
+							<button className="drop-and-file-dialog-cont__open-file-dialog-btn" onClick={() => inputEl.current.click()}>
+								Open File Dialog
+							</button>
+						</div>
+						<input
+							name="file"
+							type="file"
+							accept=".csv"
+							id="file"
+							className="input-file-cont__input-el"
+							ref={inputEl}
+							onChange={handleFileInputChange}
+						/>
+						<span className="label-for-input">Allowed Types: csv</span>
+					</>
+						
+				) : (
+					<p>{file?.name}</p>
+				)}
 					</div>
-					<input
-						name="file"
-						type="file"
-						accept=".csv"
-						id="file"
-						className="input-file-cont__input-el"
-						ref={inputEl}
-						onChange={handleFileInputChange}
-					/>
-					<span className="label-for-input">Allowed Types: csv</span>
-				</div>
 			</div>
 			<Handle
 				className="handle__input-file-node handle-right-custom-node"
